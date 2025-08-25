ec45ec0a6cc0c6a2874a5b061b2be9442b1b15a1
WafaaSh3 <fofo68619@gmail.com>
2025-08-25 15:55:12 +0300

    projectdone

--- project.py
+++ project.py
@@ -1,18 +1,16 @@
 PatientAppointments = {
-    1: ["Anas", "2023-10-01", "10:00"],
-    2: ["Sara", "2023-10-02", "11:00"],
-    3: ["Amal", "2023-10-03", "12:00"],
-    4: ["Rami", "2023-10-04", "13:00"]
+    1: ["Anas", "2023-10-01", "10:00"],2: ["Sara", "2023-10-02", "11:00"],
+    3: ["Amal", "2023-10-03", "12:00"],4: ["Rami", "2023-10-04", "13:00"]
 }
 
 while True:
     print("\nPatient Appointment Management System")
-    print("1.Add Appointment")
+    print("1.Add Appointment") 
     print("2.show Appointments")
     print("3.Delete Appointment")
     print("4.Exit")
 
-    choice = input("Choose an option (1-4): ")
+    choice = input("Choose an option (1-4):")
 
     if choice == "1":
         name = input("Enter patient name: ")
@@ -35,11 +33,9 @@ while True:
             remove_key = int(input("Enter the appointment number to delete: "))
             if remove_key in PatientAppointments:
                 PatientAppointments.pop(remove_key)
-                print("🗑️ Appointment deleted successfully!")
+                print("Appointment deleted successfully!")
             else:
-                print("⚠️ No appointment with that number.")
-            
-        
+                print("No patient with that number.")
 
     elif choice == "4":
         print("Exiting the system successfully.")
